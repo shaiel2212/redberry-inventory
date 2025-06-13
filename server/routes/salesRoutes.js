1c67c9cdbcb0e2c07e3cfc3c60895d02f99cd5f1
shaiel2212 <shay221290@gmail.com>
2025-06-13 16:36:49 +0300

    deliver_3

--- server/routes/salesRoutes.js
+++ server/routes/salesRoutes.js
@@ -6,6 +6,5 @@ const { requireAuth } = require('../middleware/authMiddleware');
 router.post('/', requireAuth, salesController.createSale);
 router.get('/', requireAuth, salesController.getAllSales);
 router.get('/:id', requireAuth, salesController.getSaleById);
-router.patch('/:id/deliver', verifyToken, salesController.markSaleAsDelivered);
 
 module.exports = router;
