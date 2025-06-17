93b08606623d68b3f001c1b778bbe2ea9e363152
shaiel2212 <shay221290@gmail.com>
2025-06-17 16:38:27 +0300

    type in handleUploadProof_2

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,26 +1,7 @@
 const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
+const { storage } = require('../config/cloudinary'); // שימוש ב-storage מ־Cloudinary
 
-
-const { storage } = require('../config/cloudinary');
-// ודא שהתיקייה קיימת
-const uploadsDir = path.join(__dirname, '../uploads');
-if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadsDir);
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const uniqueName = `proof_${Date.now()}${ext}`;
-    cb(null, uniqueName);
-  }
-});
-
+// אין צורך בתיקיית uploads מקומית
 const upload = multer({ storage });
 
 module.exports = upload;
