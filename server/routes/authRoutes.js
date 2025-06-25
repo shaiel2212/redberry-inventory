1d5de24f3c4bf3589c4f45aac50dc6a196aba74e
shaiel2212 <85456126+shaiel2212@users.noreply.github.com>
2025-06-25 09:36:12 +0300

    Update authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const { requireAuth } = require('../middleware/authMiddleware');
 
+router.post('/register', authController.registerUser);
 router.post('/login', authController.loginUser);
 router.get('/user', requireAuth, authController.getLoggedInUser);
 
